81313d0dc3ae9e9460ba8c3f7b596e47afe07c19
Julien Leroux <julienleroux9328@gmail.com>
2023-06-19 16:49:15 +0200

    express08 done

--- app.js
+++ app.js
@@ -12,30 +12,6 @@ const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
-app.get("/", welcome);
-
-const movieHandlers = require("./movieHandlers");
-
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-
-const userHandlers = require("./userHandlers");
-
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
-
-app.delete("/api/users/:id", userHandlers.deleteUser);
-
-// in app.js
-
-const { hashPassword } = require("./auth.js");
-
-app.post("/api/users", hashPassword, userHandlers.postUser);
-app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
-
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
@@ -43,3 +19,43 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
+const movieHandlers = require("./movieHandlers");
+const { hashPassword, verifyPassword, verifyToken } = require("./auth.js");
+const userHandlers = require("./userHandlers");
+
+const isItDwight = (req, res) => {
+  if (
+    req.body.email === "dwight@theoffice.com" &&
+    req.body.password === "123456"
+  ) {
+    res.send("Credentials are valid");
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+// PUBLIC
+
+app.get("/", welcome);
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+
+app.post(
+  "/api/login",
+  userHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+);
+app.post("/api/users", hashPassword, userHandlers.postUser);
+
+// PRIVATE
+
+app.use(verifyToken);
+
+app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+app.delete("/api/users/:id", userHandlers.deleteUser);
+app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
